59f50d7a2649c567a8c8dedca23af97d3045827b
syania28 <kamilaputri543@gmail.com>
2024-12-12 10:57:51 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -5,11 +5,32 @@ public class Nomor1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
+        import java.util.Scanner;
+        public class soal1 {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        /* Pekerjaan anda berakhir sini */
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
+        System.out.print("Masukan nilai N : ");
+        int n = scanner.nextInt();
+        
+        System.out.println("Deret FizzBuzz dari 0 hingga " + n + ":");
+        tampilkanDeretFizzBuzz(n); 
     }
-    
+
+    public static void tampilkanDeretFizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
+
+        
+    
